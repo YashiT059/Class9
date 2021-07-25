8bcf42701aaf15d629aa8a6f19937fc458f6ba73
YashiT059 <87928129+YashiT059@users.noreply.github.com>
2021-07-25 16:15:49 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+box =  createSprite (200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+ drawSprites ()
+ if (keyIsDown (RIGHT_ARROW)){
+   box.position.x= box.position.x + 2
+ }
+
+ if (keyDown (LEFT_ARROW)){
+   box.x = box.x -2
+ }
+
+ if (keyDown (UP_ARROW)){
+  box.y= box.y -2
+ }
+
+ if (keyDown (DOWN_ARROW)){
+  box.y= box.y + 2
+ }
+
+}
+
+
+
+
